676797da4d5923cfe8af2623fdb20b87889f71bb
ip4rsa <ip4rsa@gmail.com>
2023-10-04 23:54:55 +0330

    Update

--- lib/screens/activiti_screen.dart
+++ lib/screens/activiti_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:instagram_app/constants/colors.dart';
 
 class ActivitiScreen extends StatefulWidget {
   const ActivitiScreen({super.key});
@@ -8,13 +9,40 @@ class ActivitiScreen extends StatefulWidget {
   State<ActivitiScreen> createState() => _ActivitiScreenState();
 }
 
-class _ActivitiScreenState extends State<ActivitiScreen> {
+class _ActivitiScreenState extends State<ActivitiScreen>
+    with SingleTickerProviderStateMixin {
+  TabController? _tabController;
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Center(
-          child: Text('data'),
+        child: Container(
+          height: 70,
+          color: darkBlueColor,
+          child: TabBar(
+            controller: _tabController,
+            labelStyle: const TextStyle(
+              fontFamily: 'GB',
+              fontSize: 16,
+            ),
+            indicatorColor: pinkColor,
+            indicatorWeight: 3,
+            indicatorPadding: const EdgeInsets.symmetric(horizontal: 17),
+            tabs: const [
+              Tab(
+                text: 'You',
+              ),
+              Tab(
+                text: 'Following',
+              ),
+            ],
+          ),
         ),
       ),
     );

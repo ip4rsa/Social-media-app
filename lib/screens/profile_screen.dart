e84de10118abff0d8b676437515c37d1637ae4af
ip4rsa <ip4rsa@gmail.com>
2023-10-11 23:21:58 +0330

    Update

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -9,30 +9,54 @@ class ProfileScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: darkBlueColor,
       body: Center(
-        child: Row(
-          children: [
-            Container(
-              width: 70,
-              height: 70,
-              decoration: BoxDecoration(
-                borderRadius: const BorderRadius.all(Radius.circular(15)),
-                border: Border.all(color: pinkColor, width: 2),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(3.0),
-                child: ClipRRect(
-                  borderRadius: const BorderRadius.all(Radius.circular(12)),
-                  child: Image.asset(
-                    'assets/images/Parsa.png',
-                    fit: BoxFit.cover,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 17),
+          child: Row(
+            children: [
+              Container(
+                width: 70,
+                height: 70,
+                decoration: BoxDecoration(
+                  borderRadius: const BorderRadius.all(Radius.circular(15)),
+                  border: Border.all(color: pinkColor, width: 2),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(3.0),
+                  child: ClipRRect(
+                    borderRadius: const BorderRadius.all(Radius.circular(12)),
+                    child: Image.asset(
+                      'assets/images/Parsa.png',
+                      fit: BoxFit.cover,
+                    ),
                   ),
                 ),
               ),
-            ),
-            Column(
-              children: [],
-            )
-          ],
+              Padding(
+                padding: const EdgeInsets.only(left: 15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      'پارسا سجادیان',
+                      style: TextStyle(
+                          fontFamily: 'SM', fontSize: 12, color: wihtColor),
+                    ),
+                    const SizedBox(height: 5),
+                    Text(
+                      'ParsaSajjadian',
+                      style: TextStyle(
+                          fontFamily: 'GM', fontSize: 12, color: grayColor),
+                    ),
+                  ],
+                ),
+              ),
+              const Spacer(),
+              Image.asset(
+                'assets/images/icon_edit.png',
+              ),
+            ],
+          ),
         ),
       ),
     );

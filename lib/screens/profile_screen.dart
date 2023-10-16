976b4bb9d59e92369fc7c87f009e8236d386f223
ip4rsa <ip4rsa@gmail.com>
2023-10-16 12:14:36 +0330

    Update

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:instagram_app/constants/colors.dart';
@@ -9,154 +7,156 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: darkBlueColor,
-      body: DefaultTabController(
-        length: 2,
-        child: NestedScrollView(
-          headerSliverBuilder: (context, innerBoxIsScrolled) {
-            return [
-              SliverAppBar(
-                // pinned: true,
-                actions: const [
-                  Padding(
-                    padding: EdgeInsets.only(right: 17, top: 17),
-                    child: Icon(Icons.menu),
-                  ),
-                ],
-                bottom: PreferredSize(
-                  preferredSize: const Size.fromHeight(14),
-                  child: Container(
-                    height: 20,
-                    decoration: BoxDecoration(
-                      color: darkBlueColor,
-                      borderRadius: const BorderRadius.only(
-                        topLeft: Radius.circular(25),
-                        topRight: Radius.circular(25),
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: darkBlueColor,
+        body: DefaultTabController(
+          length: 2,
+          child: NestedScrollView(
+            headerSliverBuilder: (context, innerBoxIsScrolled) {
+              return [
+                SliverAppBar(
+                  // pinned: true,
+                  actions: const [
+                    Padding(
+                      padding: EdgeInsets.only(right: 17, top: 17),
+                      child: Icon(Icons.menu),
+                    ),
+                  ],
+                  bottom: PreferredSize(
+                    preferredSize: const Size.fromHeight(14),
+                    child: Container(
+                      height: 20,
+                      decoration: BoxDecoration(
+                        color: darkBlueColor,
+                        borderRadius: const BorderRadius.only(
+                          topLeft: Radius.circular(25),
+                          topRight: Radius.circular(25),
+                        ),
                       ),
                     ),
                   ),
-                ),
-                backgroundColor: darkBlueColor,
-                expandedHeight: 170,
-                flexibleSpace: FlexibleSpaceBar(
-                  background: Image.asset(
-                    'assets/images/item4.png',
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ),
-              SliverToBoxAdapter(
-                child: _getHeaderprofile(),
-              ),
-              SliverPersistentHeader(
-                pinned: true,
-                floating: true,
-                delegate: TabBarViewDelegate(
-                  TabBar(
-                    indicatorColor: pinkColor,
-                    indicatorWeight: 3,
-                    physics: BouncingScrollPhysics(),
-                    tabs: [
-                      Tab(
-                        icon: Image.asset('assets/images/icon_phpto.png'),
-                      ),
-                      Tab(
-                        icon: Image.asset('assets/images/icon_saved.png'),
-                      ),
-                    ],
-                  ),
-                ),
-              )
-            ];
-          },
-          body: TabBarView(
-            children: [
-              CustomScrollView(
-                slivers: [
-                  SliverPadding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 17,
-                      vertical: 20,
+                  backgroundColor: darkBlueColor,
+                  expandedHeight: 170,
+                  flexibleSpace: FlexibleSpaceBar(
+                    background: Image.asset(
+                      'assets/images/item4.png',
+                      fit: BoxFit.cover,
                     ),
-                    sliver: SliverGrid(
-                      delegate: SliverChildBuilderDelegate((context, index) {
-                        return Container(
-                          decoration: const BoxDecoration(
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(10),
-                            ),
-                          ),
-                          child: ClipRRect(
-                            borderRadius: const BorderRadius.all(
-                              Radius.circular(10),
-                            ),
-                            child: FittedBox(
-                              fit: BoxFit.cover,
-                              child: Image.asset(
-                                'assets/images/item$index.png',
+                  ),
+                ),
+                SliverToBoxAdapter(
+                  child: _getHeaderprofile(),
+                ),
+                SliverPersistentHeader(
+                  pinned: true,
+                  floating: true,
+                  delegate: TabBarViewDelegate(
+                    TabBar(
+                      indicatorColor: pinkColor,
+                      indicatorWeight: 3,
+                      physics: BouncingScrollPhysics(),
+                      tabs: [
+                        Tab(
+                          icon: Image.asset('assets/images/icon_phpto.png'),
+                        ),
+                        Tab(
+                          icon: Image.asset('assets/images/icon_saved.png'),
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+              ];
+            },
+            body: TabBarView(
+              children: [
+                CustomScrollView(
+                  slivers: [
+                    SliverPadding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 17,
+                        vertical: 20,
+                      ),
+                      sliver: SliverGrid(
+                        delegate: SliverChildBuilderDelegate((context, index) {
+                          return Container(
+                            decoration: const BoxDecoration(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(10),
                               ),
                             ),
-                          ),
-                        );
-                      }, childCount: 18),
-                      gridDelegate: SliverQuiltedGridDelegate(
-                        crossAxisCount: 3,
-                        crossAxisSpacing: 7,
-                        mainAxisSpacing: 7,
-                        repeatPattern: QuiltedGridRepeatPattern.inverted,
-                        pattern: [
-                          const QuiltedGridTile(1, 1),
-                          const QuiltedGridTile(1, 1),
-                          const QuiltedGridTile(1, 1),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              CustomScrollView(
-                slivers: [
-                  SliverPadding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 17,
-                      vertical: 20,
-                    ),
-                    sliver: SliverGrid(
-                      delegate: SliverChildBuilderDelegate((context, index) {
-                        return Container(
-                          decoration: const BoxDecoration(
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(10),
-                            ),
-                          ),
-                          child: ClipRRect(
-                            borderRadius: const BorderRadius.all(
-                              Radius.circular(10),
-                            ),
-                            child: FittedBox(
-                              fit: BoxFit.cover,
-                              child: Image.asset(
-                                'assets/images/item$index.png',
+                            child: ClipRRect(
+                              borderRadius: const BorderRadius.all(
+                                Radius.circular(10),
+                              ),
+                              child: FittedBox(
+                                fit: BoxFit.cover,
+                                child: Image.asset(
+                                  'assets/images/item$index.png',
+                                ),
                               ),
                             ),
-                          ),
-                        );
-                      }, childCount: 18),
-                      gridDelegate: SliverQuiltedGridDelegate(
-                        crossAxisCount: 1,
-                        crossAxisSpacing: 7,
-                        mainAxisSpacing: 7,
-                        repeatPattern: QuiltedGridRepeatPattern.inverted,
-                        pattern: [
-                          const QuiltedGridTile(1, 1),
-                        ],
+                          );
+                        }, childCount: 18),
+                        gridDelegate: SliverQuiltedGridDelegate(
+                          crossAxisCount: 3,
+                          crossAxisSpacing: 7,
+                          mainAxisSpacing: 7,
+                          repeatPattern: QuiltedGridRepeatPattern.inverted,
+                          pattern: [
+                            const QuiltedGridTile(1, 1),
+                            const QuiltedGridTile(1, 1),
+                            const QuiltedGridTile(1, 1),
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-            ],
+                  ],
+                ),
+                CustomScrollView(
+                  slivers: [
+                    SliverPadding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 17,
+                        vertical: 20,
+                      ),
+                      sliver: SliverGrid(
+                        delegate: SliverChildBuilderDelegate((context, index) {
+                          return Container(
+                            decoration: const BoxDecoration(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(10),
+                              ),
+                            ),
+                            child: ClipRRect(
+                              borderRadius: const BorderRadius.all(
+                                Radius.circular(10),
+                              ),
+                              child: FittedBox(
+                                fit: BoxFit.cover,
+                                child: Image.asset(
+                                  'assets/images/item$index.png',
+                                ),
+                              ),
+                            ),
+                          );
+                        }, childCount: 18),
+                        gridDelegate: SliverQuiltedGridDelegate(
+                          crossAxisCount: 1,
+                          crossAxisSpacing: 7,
+                          mainAxisSpacing: 7,
+                          repeatPattern: QuiltedGridRepeatPattern.inverted,
+                          pattern: [
+                            const QuiltedGridTile(1, 1),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
